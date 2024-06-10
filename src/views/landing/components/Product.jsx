ba6b32d89334229c4acec853a74c22079f25c828
preedok <edo.vivoy21@gmail.com>
2024-06-10 14:52:32 +0700

    update product

--- src/views/landing/components/Product.jsx
+++ src/views/landing/components/Product.jsx
@@ -67,6 +67,11 @@ const Product = () => {
         }
         return stars;
     };
+    const calculateDiscountPercentage = (price, discountPrice) => {
+        const discount = ((price - discountPrice) / price) * 100;
+        return Math.round(discount);
+    };
+
     const settings = {
         dots: true,
         infinite: true,
@@ -104,18 +109,20 @@ const Product = () => {
     }, []);
     return (
         <>
-            <div className="flex justify-between container mx-auto ">
-                <div className="text-center flex-1">
+            <div className="flex flex-col md:flex-row justify-between items-center container mx-auto px-4 py-2">
+                <div className="text-center md:flex-1 mb-4 md:mb-0">
                     <h2 data-aos="fade-up"
-                        data-aos-duration="1000" className="text-4xl font-bold text-[#8b9679] mb-4">Training Products</h2>
+                        data-aos-duration="1000" className="text-2xl md:text-3xl lg:text-4xl font-bold text-[#8b9679] mb-2">
+                        Training Products
+                    </h2>
                     <p data-aos="fade-down"
-                        data-aos-duration="1000" className="text-lg text-[#496989] mb-8">
+                        data-aos-duration="1000" className="text-sm md:text-md lg:text-lg text-[#496989]">
                         Enhance your skills with our tailored training programs.
                     </p>
                 </div>
                 <button data-aos="fade-up"
                     data-aos-duration="1000"
-                    className=" text-[#feba5b] font-bold py-2 px-3 rounded-full hover:text-blue-700 transition-colors duration-300 flex items-center ">
+                    className="text-[#feba5b] font-bold py-2 px-3 rounded-full hover:text-blue-700 transition-colors duration-300 flex items-center">
                     See All <ArrowForwardIcon fontSize='small' />
                 </button>
             </div>
@@ -123,17 +130,21 @@ const Product = () => {
                 <Slider {...settings} className="space-x-4">
                     {trainingProducts.map(product => (
                         <div key={product.id} className="px-2">
-                            <div className="bg-white shadow-lg rounded-xl overflow-hidden">
+                            <div className="bg-white shadow-lg rounded-[25px] overflow-hidden">
                                 <img data-aos="fade-up" data-aos-duration="1000" src={product.image} alt={product.name} className="w-full h-56 object-cover" />
                                 <div className="p-6">
-                                    <h3 data-aos="fade-up" data-aos-duration="1000" className="text-xl font-semibold mb-2">{product.name}</h3>
+                                    <h3 data-aos="fade-up" data-aos-duration="1000" className="text-xl text-[#124076] font-semibold mb-2">{product.name}</h3>
                                     <p data-aos="fade-up" data-aos-duration="1000" className="text-sm text-gray-500 mb-2">{product.description}</p>
-                                    <div data-aos="fade-up" data-aos-duration="1000" className="flex items-center mb-4">
+                                    <div data-aos="fade-up" data-aos-duration="1000" className="flex items-center mb-2">
                                         {renderStars(product.rating)}
                                     </div>
                                     <div data-aos="fade-up" data-aos-duration="1000" className="flex justify-between items-center mb-4">
-                                        <span className="text-xl font-bold text-gray-900"><s className="text-red-500 text-sm">Rp. {product.price.toFixed(3)}</s> <s className='text-[green]'>Rp. {product.discountPrice.toFixed(3)}</s> </span>
+                                        <span className="text-xl font-bold text-gray-900">
+                                            <s className="text-[#FFA27F] text-sm">Rp. {product.price.toFixed(3)}</s>
+                                            <span className="text-sm text-gray-500 ml-1">({calculateDiscountPercentage(product.price, product.discountPrice)}% OFF)</span>
+                                        </span>
                                     </div>
+                                    <p className="text-[#39e8a2] mt-[-20px] mb-2" style={{fontSize:'25px', fontWeight:'600'}}>Rp. {product.discountPrice.toFixed(3)}</p>
                                     <button
                                         className="w-full bg-[#feba5b] text-white font-bold py-2 px-4 rounded-full hover:bg-blue-700 transition-colors duration-300 flex justify-center items-center gap-2">
                                         <ShoppingCartIcon /> Checkout
