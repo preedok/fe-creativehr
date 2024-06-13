62831e639f2d4b416f395c1f16fd2e4a6de69932
preedok <edo.vivoy21@gmail.com>
2024-06-14 03:31:58 +0700

    update

--- src/views/landing/components/About.jsx
+++ src/views/landing/components/About.jsx
@@ -3,8 +3,9 @@ import ApartmentIcon from '@mui/icons-material/Apartment';
 import LocalLibraryIcon from '@mui/icons-material/LocalLibrary';
 import OndemandVideoIcon from '@mui/icons-material/OndemandVideo';
 import CastForEducationIcon from '@mui/icons-material/CastForEducation';
-import { FaCheckCircle } from 'react-icons/fa'; // atau impor ikon lainnya sesuai kebutuhan
+import { FaCheckCircle } from 'react-icons/fa'; 
 import icon2 from '../../../assets/icon2.gif'
+import ReactPlayer from 'react-player';
 import Slider from 'react-slick';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -19,7 +20,7 @@ const AboutUs = () => {
         slidesToShow: 3,
         slidesToScroll: 1,
         autoplay: true,
-        autoplaySpeed: 1000,
+        autoplaySpeed: 4000,
         responsive: [
             {
                 breakpoint: 1024,
@@ -29,7 +30,7 @@ const AboutUs = () => {
                     infinite: true,
                     dots: true,
                     autoplay: true,
-                    autoplaySpeed: 3000
+                    autoplaySpeed: 4000
                 }
             },
             {
@@ -38,7 +39,7 @@ const AboutUs = () => {
                     slidesToShow: 1,
                     slidesToScroll: 1,
                     autoplay: true,
-                    autoplaySpeed: 3000
+                    autoplaySpeed: 4000
                 }
             }
         ]
@@ -46,11 +47,11 @@ const AboutUs = () => {
 
     const cardData = [
         {
-            icon: <LocalLibraryIcon fontSize="large" />, title: "Regular Training", subtitle: "Pelatihan secara rutin untuk pengembangan keterampilan.", link: "#inovasi"
+            icon: <LocalLibraryIcon fontSize="large" />, title: "Regular Training", subtitle: "Pelatihan secara rutin untuk pengembangan keterampilan.", videoUrl: "https://youtu.be/8X2ohD8LvuM?si=UyOYPc0ZFpqHsL1R"
         },
-        { icon: <ApartmentIcon fontSize="large" />, title: "In House Training", subtitle: "Pelatihan yang diselenggarakan di dalam perusahaan atau organisasi.", link: "#kebahagiaan" },
-        { icon: <OndemandVideoIcon fontSize="large" />, title: "Video Traning", subtitle: "Pelatihan yang dapat diakses melalui video untuk kemudahan akses.", link: "#kualitas" },
-        { icon: <CastForEducationIcon fontSize="large" />, title: "Webinar", subtitle: "Sesi pelatihan online dengan pemateri ahli di bidangnya.", link: "#passion" },
+        { icon: <ApartmentIcon fontSize="large" />, title: "In House Training", subtitle: "Pelatihan yang diselenggarakan di dalam perusahaan atau organisasi.", videoUrl: "https://youtu.be/7Prm9D80yfg?si=BsUJaW_bHSGbCc9K" },
+        { icon: <OndemandVideoIcon fontSize="large" />, title: "Video Training", subtitle: "Pelatihan yang dapat diakses melalui video untuk kemudahan akses.", videoUrl: "https://youtu.be/x216BSqkoTw?si=5-_oc6HOEOMqbfRN" },
+        { icon: <CastForEducationIcon fontSize="large" />, title: "Webinar", subtitle: "Sesi pelatihan online dengan pemateri ahli di bidangnya.", videoUrl: "https://youtu.be/J0Dc3FFEXdM?si=pABLXEKOxT6CFUUx" },
     ];
 
     useEffect(() => {
@@ -68,7 +69,35 @@ const AboutUs = () => {
                         Di Creativehr, kami berdedikasi untuk mendorong pertumbuhan dan pengembangan melalui solusi pelatihan yang inovatif. Misi kami adalah menyediakan pengalaman belajar terbaik, layanan konsultasi, dan program transformasi untuk bisnis, pemerintah, dan individu. Bergabunglah dengan kami dalam perjalanan peningkatan dan kesuksesan yang berkelanjutan.
                     </p>
                 </div>
-                <div className="w-full max-w-7xl mx-auto px-4">
+                <div className="w-full max-w-7xl mx-auto rounded-[20px]">
+                    <Slider {...settings}>
+                        {cardData.map((card, index) => (
+                            <div key={index} className="p-4 relative">
+                                <div className="bg-white bg-opacity-70 shadow-lg  p-8 m-4 flex flex-col items-center relative">
+                                    {/* React Player untuk video latar belakang */}
+                                    <ReactPlayer
+                                        url={card.videoUrl}
+                                        playing={true}
+                                        loop={true}
+                                        muted={true}
+                                        width="100%"
+                                        height="100%"
+                                        style={{ position: 'absolute', top: 0, left: 0, zIndex: -1 }}
+                                        className="rounded-[25px]"
+                                    />
+                                    {/* Konten teks dan ikon di atas video */}
+                                    <div className="z-10 relative text-center">
+                                        <div className="text-5xl text-gray-800 mb-4">{card.icon}</div>
+                                        <h3 className="text-2xl font-bold text-gray-800 mb-2">{card.title}</h3>
+                                        <p className="text-gray-600 mb-4">{card.subtitle}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        ))}
+                    </Slider>
+                </div>
+
+                {/* <div className="w-full max-w-7xl mx-auto px-4">
                     <Slider {...settings}>
                         {cardData.map((card, index) => (
                             <div key={index} className="p-4">
@@ -87,7 +116,7 @@ const AboutUs = () => {
                             </div>
                         ))}
                     </Slider>
-                </div>
+                </div>  */}
             </section>
             <section className="py-5">
                 <div className="container mx-auto px-4 flex flex-wrap">
